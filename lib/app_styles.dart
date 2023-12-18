67fbdc749063607a279d936324a942ba7c924239
SK Sadia Tasnim Elma <114817868+Elma17@users.noreply.github.com>
2023-12-19 00:26:01 +0600

    Add files via upload

---
+++ lib/app_styles.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+class AppStyles {
+  static const TextStyle nameStyle = TextStyle(
+    color: Colors.white,
+    fontSize: 18.0,
+    fontWeight: FontWeight.bold,
+  );
+
+  static const TextStyle emailStyle = TextStyle(
+    color: Colors.white,
+    fontSize: 14.0,
+  );
+
+  static const TextStyle tasksHeadingStyle = TextStyle(
+    fontSize: 20.0,
+    fontWeight: FontWeight.bold,
+  );
+
+  static const TextStyle tasksStyle = TextStyle(
+    fontSize: 16.0,
+  );
+}
